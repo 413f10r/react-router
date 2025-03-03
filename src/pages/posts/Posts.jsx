b45d95525ad58bb5220dd20de3a514a870595a40
413f10r <ale-fioretti@live.it>
2025-03-03 22:48:38 +0100

    add: posts

--- src/pages/posts/Posts.jsx
+++ src/pages/posts/Posts.jsx
@@ -1,14 +1,50 @@
 //compoennts
+import { useState, useEffect } from 'react';
+// import axios from 'axios';
+import { NavLink } from 'react-router-dom';
 
-const Posts = () => {
+const Products = () => {
+  const [products, setProducts] = useState([]);
+
+  const url = import.meta.env.VITE_ENDPOINT_URL;
+
+  useEffect(() => {
+    console.log(url);
+  });
+
+  useEffect(() => {
+    // axios
+    //   .get(url)
+    //   .then((res) => setProducts(res.data));
+
+    fetch(`${url}`, { method: 'GET' })
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
+
   return (
     <>
-      <div className="mainCont">
-        <h1>Post</h1>
+      <div className="container">
+        <div className="row">
+          {products.map((elem) => {
+            return (
+              <div key={elem.id} className="col-4">
+                <NavLink to={`/products/${elem.id}`}>
+                  <div className="card">
+                    <div className="card-body">
+                      <h4 className="card-title">{elem.name}</h4>
+                      <p className="card-text">{elem.ingredients}</p>
+                    </div>
+                  </div>
+                </NavLink>
+              </div>
+            );
+          })}
+        </div>
       </div>
-
     </>
   );
 };
 
-export default Posts;
+export default Products;
+
